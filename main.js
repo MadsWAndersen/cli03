1cc677c17b9dda7a4efb1e88048b68d95e06c442
Mads Welling <madswandersen@hotmail.com>
2023-01-31 11:08:54 +0100

    Added md

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ import { marked } from "marked";
 document.getElementById("preview").innerHTML = marked.parse(
   "# Marked in the browser\n\nRendered by **marked**."
 );
+
+let btn = document.getElementById("content");
+
+btn.addEventListener("input", function () {
+  let value = document.getElementById("content").value;
+  let change = document.getElementById("preview");
+  change.innerHTML = marked.parse(value);
+});
